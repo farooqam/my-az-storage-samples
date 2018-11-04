9d28dff68ea460f5b58fb4a2fcf1b6879ccfa741
farooqam <farooq_am@hotmail.com>
2018-11-04 01:19:35 -0700

    Initial commit.

---
+++ AzStorageSamples/BlobUploader/CommandLine/Options.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+namespace My.AzureStorage.Samples.Lib.BlobUploader.CommandLine
+{
+    public class Options
+    {
+        [Option(Required = true, HelpText = "Azure storage connection string.")]
+        public string StorageConnectionString { get; set; }
+
+        [Option(Required = true, HelpText = "Azure storage container name.")]
+        public string ContainerName { get; set; }
+
+        [Option('f', Required = true, HelpText = "File to upload.")]
+        public string FilePath { get; set; }
+
+        [Option(Required = false, HelpText = "The file's HTTP content-type.")]
+        public string ContentType { get; set; }
+    }
+}
---
+++ AzStorageSamples/BlobUploader/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using CommandLine;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.RetryPolicies;
+using My.AzureStorage.Samples.Lib.BlobUploader.CommandLine;
+
+namespace My.AzureStorage.Samples.Lib.BlobUploader
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Options parsedOptions = null;
+            Parser.Default.ParseArguments<Options>(args).WithParsed(options => { parsedOptions = options; });
+            await Run(parsedOptions);
+        }
+
+        static async Task Run(Options options)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(options.StorageConnectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(options.ContainerName);
+            BlobRequestOptions requestOptions = new BlobRequestOptions() { RetryPolicy = new ExponentialRetry(TimeSpan.FromSeconds(5), 3) };
+            await container.CreateIfNotExistsAsync(requestOptions, null);
+
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(Path.GetFileName(options.FilePath));
+            blockBlob.Properties.ContentType = options.ContentType;
+            await blockBlob.UploadFromFileAsync(options.FilePath);
+        }
+    }
+}
